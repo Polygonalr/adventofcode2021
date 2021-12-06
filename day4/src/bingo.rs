599bcd47838315d5b7618745177d61612b0f304b
Zhi Heng <yapzhhg@gmail.com>
2021-12-07 07:38:16 +0800

    Day 4

---
+++ day4/src/bingo.rs
@@ -0,0 +1,63 @@
+pub mod bingo_grid {
+    use std::fmt;
+
+    #[derive(Debug, Clone)]
+    pub struct BingoGrid {
+        number_grid: [[i32; 5]; 5],
+        marked_grid: [[bool; 5]; 5],
+    }
+
+    impl BingoGrid {
+        pub fn new(number_grid: [[i32; 5]; 5]) -> BingoGrid {
+            BingoGrid { 
+                number_grid: number_grid,
+                marked_grid: [[false; 5]; 5],
+            }
+        }
+        // set marked_grid to true given a winning number
+        // returns true if the number exists, false if not.
+        pub fn add_winning_number(&mut self, winning_number: i32) -> bool {
+            for i in 0..5 {
+                for j in 0..5 {
+                    if self.number_grid[i][j] == winning_number {
+                    self.marked_grid[i][j] = true;
+                    return true;
+                    }
+                }
+            }
+            false
+        }
+        // check whether there is any row or column in marked_grid full of true
+        pub fn is_bingo(&self) -> bool {
+            for i in 0..5 {
+                if self.marked_grid[i].iter().all(|&x| x) {
+                    return true;
+                }
+            }
+            for j in 0..5 {
+                if self.marked_grid.iter().map(|row| row[j]).all(|x| x) {
+                    return true;
+                }
+            }
+            false
+        }
+        // sum of all non-winning numbers in number_grid
+        pub fn sum_of_non_winning_numbers(&self) -> i32 {
+            let mut sum = 0;
+            for i in 0..5 {
+                for j in 0..5 {
+                    if !self.marked_grid[i][j] {
+                        sum += self.number_grid[i][j];
+                    }
+                }
+            }
+            sum
+        }
+    }
+
+    impl fmt::Display for BingoGrid {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Bingo Grid: {:?}\n", self.number_grid)
+        }
+    }
+}
---
+++ day4/src/main.rs
@@ -0,0 +1,126 @@
+/*
+    OOP Solution, might be inefficient but this is a modular way to solve this question.
+*/
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+mod bingo;
+pub use crate::bingo::bingo_grid::BingoGrid;
+
+fn p1(mut bingo_grids: Vec<BingoGrid>, winning_numbers: Vec<i32>) -> i32 {
+    for i in 0..4 {
+        for j in 0..bingo_grids.len() {
+            bingo_grids[j].add_winning_number(winning_numbers[i]);
+        }
+    }
+    let (mut i, mut index) = (4, 0);
+    let mut current_winning_number: i32;
+    let mut done = false;
+    loop {
+        current_winning_number = winning_numbers[i];
+        for j in 0..bingo_grids.len() {
+            if bingo_grids[j].add_winning_number(current_winning_number) 
+            && bingo_grids[j].is_bingo() {
+                index = j;
+                done = true;
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+        i += 1;
+    }
+    bingo_grids[index].sum_of_non_winning_numbers() * current_winning_number
+}
+
+fn p2(mut bingo_grids: Vec<BingoGrid>, winning_numbers: Vec<i32>) -> i32 {
+    for i in 0..4 {
+        for j in 0..bingo_grids.len() {
+            bingo_grids[j].add_winning_number(winning_numbers[i]);
+        }
+    }
+    let mut i = 4;
+    let mut current_winning_number: i32;
+    loop {
+        current_winning_number = winning_numbers[i];
+        let mut indexes_to_remove: Vec<usize> = Vec::new();
+        for j in 0..bingo_grids.len() {
+            if bingo_grids[j].add_winning_number(current_winning_number) 
+            && bingo_grids[j].is_bingo() {
+                indexes_to_remove.push(j);
+            }
+        }
+        indexes_to_remove.reverse();
+        for j in indexes_to_remove {
+            bingo_grids.remove(j);
+        }
+        if bingo_grids.len() == 1 {
+            break;
+        }
+        i += 1;
+    }
+
+    loop {
+        current_winning_number = winning_numbers[i];
+        if bingo_grids[0].add_winning_number(current_winning_number) 
+        && bingo_grids[0].is_bingo() {
+            break;
+        }
+        i += 1;
+    }
+
+    bingo_grids[0].sum_of_non_winning_numbers() * current_winning_number
+}
+
+fn main() {
+    let filepath = "./input.txt";
+    let mut winning_numbers: Vec<i32> = Vec::new();
+    let mut bingo_grids: Vec<BingoGrid> = Vec::new();
+    let (mut first_line_done, mut second_line_done) = (false, false);
+    let mut bingo_buffer: [[i32; 5]; 5] = [[0; 5]; 5];
+    let mut current_row = 0;
+
+    if let Ok(lines) = read_lines(filepath) {
+        for line in lines {
+            if !first_line_done {
+                if let Ok(s) = line {
+                    let s_vec: Vec<i32> = s.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+                    winning_numbers = s_vec.to_vec();
+                    first_line_done = true;
+                }
+            } else if !second_line_done {
+                second_line_done = true;
+                continue;
+            } else if let Ok(s) = line {
+                // store bingo grids
+                if s == "" {
+                    let new_bingo = BingoGrid::new(bingo_buffer);
+                    bingo_grids.push(new_bingo);
+                    bingo_buffer = [[0; 5]; 5];
+                    current_row = 0;
+                    continue;
+                }
+                let s_vec: Vec<i32> = s.split(" ").filter(|&x| x != "")
+                        .map(|x| x.parse::<i32>().unwrap()).collect();
+                for i in 0..s_vec.len() {
+                    bingo_buffer[current_row][i] = s_vec[i];
+                }
+                current_row += 1;
+            }
+        }
+    }
+    // let line_vec2 = line_vec.to_vec();
+    println!("Part 1: {}\nPart 2: {}", p1(bingo_grids.to_vec(), winning_numbers.to_vec()),
+                                       p2(bingo_grids.to_vec(), winning_numbers.to_vec()));
+}
+
+// Reusable function to read files
+// From: https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
