67873d1420280ea205e6a3ea752421e5171dbd52
Zhi Heng <yapzhhg@gmail.com>
2021-12-20 15:52:01 +0800

    Day 5

---
+++ day5/src/line.rs
@@ -0,0 +1,131 @@
+pub mod line {
+    use std::{fmt, mem};
+    use std::collections::HashSet;
+    use std::cmp::{min, max};
+
+    #[derive(Debug, Clone)]
+    pub struct Line {
+        start_x: i32,
+        end_x: i32,
+        start_y: i32,
+        end_y: i32
+    }
+
+    impl Line {
+        pub fn new(mut start_x: i32, mut end_x: i32, mut start_y: i32, mut end_y: i32) -> Line {
+            // For simplicity sake, make every line start from left and end at right.
+            // If it is a vertical line, make it start from down and end at up.
+            // therefore, start_x < end_x, and if start_x = end_x, then start_y < end_y 
+            if start_x > end_x || (start_x == end_x && start_y > end_y) {
+                mem::swap(&mut start_x, &mut end_x);
+                mem::swap(&mut start_y, &mut end_y);
+            }
+            Line {
+                start_x: start_x,
+                end_x: end_x,
+                start_y: start_y,
+                end_y: end_y
+            }
+        }
+
+        // check whether 2 lines overlap
+        pub fn is_overlap(&self, other: &Line) -> bool {
+            let x_overlap = (self.start_x <= other.start_x && self.end_x >= other.start_x) ||
+                            (self.start_x <= other.end_x && self.end_x >= other.end_x) ||
+                            (other.start_x <= self.start_x && other.end_x >= self.start_x) ||
+                            (other.start_x <= self.end_x && other.end_x >= self.end_x);
+
+            let y_overlap = (self.start_y <= other.start_y && self.end_y >= other.start_y) ||
+                            (self.start_y <= other.end_y && self.end_y >= other.end_y) ||
+                            (other.start_y <= self.start_y && other.end_y >= self.start_y) ||
+                            (other.start_y <= self.end_y && other.end_y >= self.end_y);
+
+            x_overlap && y_overlap
+        }
+
+        // Find and add intersection points of 2 lines to intersection_set and return
+        pub fn add_intersections(&self, other: &Line, intersection_set: HashSet<(i32,i32)>) -> Result<HashSet<(i32, i32)>, &'static str> {
+            if !self.is_overlap(other) {
+                return Err("No intersection");
+            }
+            if !self.is_diagonal() && !other.is_diagonal() {
+                // non-diagonal lines
+                let mut new_intersection_set: HashSet<(i32, i32)> = intersection_set.clone();
+
+                // Check whether they are parallel first
+                let parallel_x = self.start_x == self.end_x && other.start_x == other.end_x;
+                let parallel_y = self.start_y == self.end_y && other.start_y == other.end_y;
+                if parallel_x {
+                    // println!("Parallel (x) lines found: {} and {}", self, other);
+                    let (start_y, end_y) = (max(self.start_y, other.start_y), min(self.end_y, other.end_y));
+                    let fixed_x = self.start_x;
+                    for i in start_y..(end_y + 1) {
+                        new_intersection_set.insert((fixed_x, i));
+                    }
+                } else if parallel_y {
+                    // println!("Parallel (y) lines found: {} and {}", self, other);
+                    let (start_x, end_x) = (max(self.start_x, other.start_x), min(self.end_x, other.end_x));
+                    let fixed_y = self.start_y;
+                    for i in start_x..(end_x + 1) {
+                        new_intersection_set.insert((i, fixed_y));
+                    }
+                } else {
+                    // insert intersection point of a horizontal and vertical line
+                    // check whether self is horizontal
+                    let (intersect_x, intersect_y): (i32, i32);
+                    if self.start_y == self.end_y {
+                        // other must be vertical
+                        intersect_x = other.start_x;
+                        intersect_y = self.start_y;
+                    } else {
+                        intersect_x = self.start_x;
+                        intersect_y = other.start_y;
+                    }
+                    new_intersection_set.insert((intersect_x, intersect_y));
+                }
+
+                return Ok(new_intersection_set);
+            }
+            Err("Diagonal lines detected, use naive_compile_points instead")
+        }
+
+        pub fn is_diagonal(&self) -> bool {
+            self.start_x != self.end_x && self.start_y != self.end_y
+        }
+
+        pub fn naive_compile_points(&self) -> Vec<(i32, i32)> {
+            let mut all_points: Vec<(i32, i32)> = Vec::new();
+            if !self.is_diagonal() {
+                if self.start_x == self.end_x {
+                    for i in self.start_y..(self.end_y + 1) {
+                        all_points.push((self.start_x, i));
+                    }
+                } else {
+                    for i in self.start_x..(self.end_x + 1) {
+                        all_points.push((i, self.start_y));
+                    }
+                }
+            } else {
+                let dx = if self.start_x < self.end_x { 1 } else { -1 };
+                let dy = if self.start_y < self.end_y { 1 } else { -1 };
+                let mut x = self.start_x;
+                let mut y = self.start_y;
+                loop {
+                    all_points.push((x, y));
+                    x += dx;
+                    y += dy;
+                    if x == self.end_x + dx {
+                        break;
+                    }
+                }
+            }
+            all_points
+        }
+    }
+
+    impl fmt::Display for Line {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Line: ({}, {}) to ({}, {})", self.start_x, self.start_y, self.end_x, self.end_y)
+        }
+    }
+}
---
+++ day5/src/main.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::{HashSet, HashMap};
+
+mod line;
+
+pub use crate::line::line::Line;
+
+// Overcomplicated O(nlogn) but memory efficient solution which computes the overlapping points only
+fn p1(line_vec: Vec<String>) -> i32 {
+    // replace all the line's ' -> ' to ',' and then split with ','
+    let new_line_vec: Vec<Vec<String>> = line_vec.iter()
+        .map(|x| x.replace(" -> ", ",")
+        .split(",")
+        .map(|x| x.to_owned()).collect())
+        .collect();
+    // convert to Vec<Vec<i32>> and filter only horizontal or vertical lines
+    let filtered_vec: Vec<Vec<i32>> = new_line_vec.iter()
+        .map(|x| x.iter().map(|y| y.parse::<i32>().unwrap()).collect())
+        .filter(|x: &Vec<i32>| x[0] == x[2] || x[1] == x[3])
+        .collect();
+    // convert to Vec<Line>
+    let filtered_line_vec: Vec<Line> = filtered_vec.iter()
+        .map(|x| Line::new(x[0], x[2], x[1], x[3]))
+        .collect();
+    let mut intersection_set: HashSet<(i32,i32)> = HashSet::new();
+    for i in 0..(filtered_line_vec.len() - 1) {
+        for j in (i + 1)..filtered_line_vec.len() {
+            if filtered_line_vec[i].is_overlap(&filtered_line_vec[j]) {
+                intersection_set = filtered_line_vec[i].add_intersections(&filtered_line_vec[j], intersection_set).unwrap();
+            }
+        }
+    }
+    intersection_set.len() as i32
+}
+
+// Naive "store all the points" solution
+fn p2(line_vec: Vec<String>) -> i32 {
+    let mut points: Vec<(i32, i32)> = Vec::new();
+    // replace all the line's ' -> ' to ',' and then split with ','
+    let new_line_vec: Vec<Vec<String>> = line_vec.iter()
+        .map(|x| x.replace(" -> ", ",")
+        .split(",")
+        .map(|x| x.to_owned()).collect())
+        .collect();
+    // convert to Vec<Vec<i32>>
+    let filtered_vec: Vec<Vec<i32>> = new_line_vec.iter()
+        .map(|x| x.iter().map(|y| y.parse::<i32>().unwrap()).collect())
+        .collect();
+    // convert to Vec<Line>
+    let line_obj_vec: Vec<Line> = filtered_vec.iter()
+        .map(|x| Line::new(x[0], x[2], x[1], x[3]))
+        .collect();
+    // Add each point of every line to the points vec
+    for line in line_obj_vec.iter() {
+        points.append(&mut line.naive_compile_points());
+    }
+    // Convert the vec into a HashMap of frequency of how many times each points appears
+    let mut freq = points.iter().copied().fold(HashMap::new(), |mut map, val|{
+        map.entry(val).and_modify(|frq|*frq += 1).or_insert(1);
+        map
+    });
+    // Remove points which only shows up less than 2 times
+    freq.retain(|_, e| *e > 1);
+    freq.len() as i32
+}
+
+fn main() {
+    let filepath = "./input.txt";
+    // let mut str_buf = "".to_owned();
+    let mut line_vec: Vec<String> = Vec::new();
+    if let Ok(lines) = read_lines(filepath) {
+        for line in lines {
+            if let Ok(s) = line {
+                // Process each line...
+                line_vec.push(s);
+            }
+        }
+    }
+    let line_vec2 = line_vec.to_vec();
+    println!("Part 1: {}\nPart 2: {}", p1(line_vec), p2(line_vec2));
+}
+
+// Reusable function to read files
+// From: https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
